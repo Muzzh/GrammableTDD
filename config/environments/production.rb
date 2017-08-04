842829504a2a1f594f23ba7e1033d20ce250a104
Muzzh <murdyn@gmail.com>
2017-08-04 14:57:49 +0000

    added devise gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,6 +52,9 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
+  # devise mailer
+  config.action_mailer.default_url_options = { host: 'https://grammable-xavier-vanier.herokuapp.com/
+' }
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "grammable_#{Rails.env}"
