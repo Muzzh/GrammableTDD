c5def059e35e2308e046a6535cde270c43eab3a2
Muzzh <murdyn@gmail.com>
2017-08-07 16:48:34 +0000

    adding comments (pre-fixing tests)

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -171,4 +171,29 @@ RSpec.describe GramsController, type: :controller do
       expect(response).to have_http_status(:not_found)
     end
   end
+
+  describe 'comments#create action' do
+    it 'should allow users to create comments on grams' do
+      gram = FactoryGirl.create(:gram)
+      sign_in gram.user
+      post :create, params: { gram_id: gram.id, comment: { message: 'awesome gram'} }
+      expect(response).to redirect_to root_path
+      expect(gram.comments.length).to eq 1
+      expect(gram.comments.first.message).to eq 'awesome gram'
+    end
+
+    it 'should require a user to be logged in to comment on a gram' do
+      gram = FactoryGirl.create(:gram)
+      post :create, params: { gram_id: gram.id, comment: { message: 'POODLES!' } }
+      expect(response).to redirect_to new_user_session_path
+      expect(gram.message.length).to eq 0
+    end
+
+    it 'should return a 404 error if the gram cannot be found' do
+      user = FactoryGirl.create(:user)
+      sign_in user
+      post :create, params: { gram_id: 'YOLOSWAG', comment: { message: 'yoloswag' } }
+      expect(response).to have_http_status(:not_found)
+    end
+  end
 end
